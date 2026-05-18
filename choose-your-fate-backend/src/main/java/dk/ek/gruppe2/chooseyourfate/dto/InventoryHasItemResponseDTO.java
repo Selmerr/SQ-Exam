55ee4dbfbd30af96e87e038bf1e869ff9c80403a
Patrick Fabrin <pf@dafolo.dk>
2026-05-18 12:35:16 +0200

    fixed minor issue with a missing contructor in a DTO

--- choose-your-fate-backend/src/main/java/dk/ek/gruppe2/chooseyourfate/dto/InventoryHasItemResponseDTO.java
+++ choose-your-fate-backend/src/main/java/dk/ek/gruppe2/chooseyourfate/dto/InventoryHasItemResponseDTO.java
@@ -1,6 +1,5 @@
 package dk.ek.gruppe2.chooseyourfate.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +7,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class InventoryHasItemResponseDTO {
 
+    private Integer inventoryId;
     private Integer amount;
     private ItemResponseDTO item;
+
+    public InventoryHasItemResponseDTO(Integer amount, ItemResponseDTO item) {
+        this.amount = amount;
+        this.item = item;
+    }
+
+    public InventoryHasItemResponseDTO(Integer inventoryId, Integer amount, ItemResponseDTO item) {
+        this.inventoryId = inventoryId;
+        this.amount = amount;
+        this.item = item;
+    }
 }
