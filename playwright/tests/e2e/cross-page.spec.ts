dfdb0adb74e9bede025ebd1fc322dd3a150762ea
Sekionn <Sjknudesn@gmail.com>
2026-05-29 17:14:17 +0200

    Add notes for register tests that are cross page tests

--- playwright/tests/e2e/cross-page.spec.ts
+++ playwright/tests/e2e/cross-page.spec.ts
@@ -7,3 +7,8 @@ test.beforeEach(async ({ page }) => {
 test('test', async ({ page }) => {
   
 });
+
+//Create test that confirms that when you enter the base page (login page), you can switch to the register page by using the button.
+//Create a test of the reverse scenario i.e from register page to login page. THEY MUST NOT BE THE SAME TEST!!!
+
+//Create test that creates new account on register page and then is capable of logging in with that same account after being redirected to the login page and that the account page is then opened.
