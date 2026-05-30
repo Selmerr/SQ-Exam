90a969ba8cd5c5273c5b978b8d67dc7663b9f01a
Mikkel Olsen <molybdenum93@gmail.com>
2026-05-30 22:29:22 +0200

    Revert "fix"
    This reverts commit 44b423266c99c59aeb4194b6244007ed1a6961bd.

--- playwright/tests/debug.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test';
-
-test('debug env', async () => {
-    console.log('API_URL:', process.env.API_URL);
-    console.log('ADMIN_TOKEN:', process.env.ADMIN_TOKEN?.substring(0, 20));
-});
