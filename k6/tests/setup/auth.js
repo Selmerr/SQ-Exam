4a82b7215c5f0a52a9c8e243fc055921552fbf77
Sebastian Juul Knudsen <Sjknudesn@gmail.com>
2026-05-31 14:13:47 +0200

    remove change

--- k6/tests/setup/auth.js
+++ k6/tests/setup/auth.js
@@ -9,8 +9,8 @@ const BASE_URL = __ENV.BASE_URL || 'http://localhost:8080';
  */
 export function getToken() {
   const credentials = {
-    username: "admin",
-    password: "admin123"
+    username: __ENV.K6_USERNAME,
+    password: __ENV.K6_PASSWORD
   };
 
   const res = http.post(
